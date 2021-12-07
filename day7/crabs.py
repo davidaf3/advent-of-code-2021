a8ed9a73ac9ee0573b04bab39aeed252f45beb18
davidaf3 <david.alvarez.fidalgo@gmail.com>
2021-12-07 17:43:47 +0100

    Day 7

---
+++ day7/crabs.py
@@ -0,0 +1,40 @@
+from typing import Callable
+
+def main():
+    with open('input.txt') as f:
+        numbers = f.readline()
+    crabs = [int(number) for number in numbers.split(',')]
+    min_position = min(crabs)
+    max_position = max(crabs)
+    calculator = FuelCalculator()
+    calculator.precompute_values(max_position)
+    cost_function1 = lambda x: sum(abs(x - position) for position in crabs)
+    cost_function2 = lambda x: sum(calculator.fuel(abs(x - position)) for position in crabs)
+    print(find_min(cost_function1, min_position, max_position)[1])
+    print(find_min(cost_function2, min_position, max_position)[1])
+
+def find_min(f: Callable[[int], int], start: int, end: int):
+    min_x = start
+    min_y = f(min_x)
+    for x in range(start + 1, end + 1):
+        if (y := f(x)) < min_y:
+            min_x = x
+            min_y = y
+    return min_x, min_y
+
+class FuelCalculator(object):
+
+    def __init__(self) -> None:
+        self.memory = {}
+
+    def precompute_values(self, max_distance: int) -> None:
+        fuel = 0
+        for i in range(max_distance + 1):
+            fuel += i
+            self.memory[i] = fuel
+
+    def fuel(self, distance: int):
+        return self.memory[distance]
+
+if __name__ == '__main__':
+    main()
